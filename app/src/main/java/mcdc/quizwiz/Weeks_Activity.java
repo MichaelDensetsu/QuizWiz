33fa5bb5b6c63a017a9c3dfab8c60a48ec69a3a8
Dennis Chen <z5118055@ad.unsw.edu.au>
2018-10-26 20:05:13 +1100

    Update

--- app/src/main/java/mcdc/quizwiz/Weeks_Activity.java
+++ app/src/main/java/mcdc/quizwiz/Weeks_Activity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.barteksc.pdfviewer.PDFView;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -18,6 +19,7 @@ import com.google.android.youtube.player.YouTubeStandalonePlayer;
 
 public class Weeks_Activity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
+
     static final String GOOGLE_API_KEY = "AIzaSyAsWkRZuVHwVO7HkN6SjOmzHqrLdN-2Hos";
     static final String YOUTUBE_VIDEO_ID = "9TycLR0TqFA";
     static final String YOUTUBE_VIDEO_ID2 = "sobxOzRjAGg";
@@ -25,42 +27,49 @@ public class Weeks_Activity extends YouTubeBaseActivity implements YouTubePlayer
 
     private static final String TAG = "YoutubeActivity";
 
+    Toolbar toolbar2;
+    PDFView pdfView;
 
     YouTubePlayerView playerView;
-
-    TextView textView;
-    Toolbar toolbar2;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weeks_);
 
+
         toolbar2 = (Toolbar) findViewById(R.id.toolbar2);
 
+        pdfView = (PDFView) findViewById(R.id.pdf);
 
-        TextView textView = (TextView) findViewById(R.id.textView);
 
-
+        playerView = (YouTubePlayerView) findViewById(R.id.player);
 
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             toolbar2.setTitle(bundle.getString("Topic"));
             if (toolbar2.getTitle().toString().equalsIgnoreCase("Agile Scrum")) {
-                YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
+
                 playerView.initialize(GOOGLE_API_KEY, this);
-                textView.setText("Test 1");
+
+                pdfView.fromAsset("scrum.pdf")
+                        .load();
+
 
             } else if (toolbar2.getTitle().toString().equalsIgnoreCase("Lean Start-up")) {
-                YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
+                //YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
                 playerView.initialize(GOOGLE_API_KEY, this);
-                textView.setText("Test 2");
+
+                pdfView.fromAsset("lean.pdf")
+                        .load();
+
 
             } else if (toolbar2.getTitle().toString().equalsIgnoreCase("Design Thinking")) {
-                YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
+                // YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
                 playerView.initialize(GOOGLE_API_KEY, this);
-                textView.setText("Test 3");
+
+                pdfView.fromAsset("design.pdf")
+                        .load();
 
 
             }
@@ -76,13 +85,12 @@ public class Weeks_Activity extends YouTubeBaseActivity implements YouTubePlayer
         Log.d(TAG, "onInitializationSuccess: provider is " + provider.getClass().toString());
         Toast.makeText(this, "Initialised Youtube Player Successfully", Toast.LENGTH_LONG).show();
 
-        if (toolbar2.getTitle().toString().equalsIgnoreCase("Week 1")) {
+        if (toolbar2.getTitle().toString().equalsIgnoreCase("Agile Scrum")) {
             youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
 
-        } else if (toolbar2.getTitle().toString().equalsIgnoreCase("Week 2")) {
+        } else if (toolbar2.getTitle().toString().equalsIgnoreCase("Lean Start-up")) {
             youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID2);
-        }
-        else youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID3);
+        } else youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID3);
     }
 
     @Override
@@ -93,7 +101,7 @@ public class Weeks_Activity extends YouTubeBaseActivity implements YouTubePlayer
         if (youTubeInitializationResult.isUserRecoverableError()) {
             youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show();
         } else {
-            String errorMessage=String.format("There was an error initialising the YoutubePlayer (%1$s)", youTubeInitializationResult.toString());
+            String errorMessage = String.format("There was an error initialising the YoutubePlayer (%1$s)", youTubeInitializationResult.toString());
             Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
 
         }
